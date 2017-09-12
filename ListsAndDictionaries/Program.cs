926093efb47105bb5c8ebee4d189f59ebafb94b5
Lily Krichels <lily.krichels@gmail.com>
2017-09-12 16:16:09 -0400

    First Commit

---
+++ ListsAndDictionaries/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListsAndDictionaries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //List<string> faveFoods = new List<string>() { "pizza", "alfredo", "chicken", "sushi", "broccoli" };
+            //Console.WriteLine(faveFoods[0]);
+            //Console.WriteLine(faveFoods[4]);
+
+            //List<string> badFoods = new List<string>();
+            //badFoods[0] = "Onions";
+            //badFoods[1] = "Mushrooms";
+            //badFoods[2] = "Liver";
+            //badFoods[3] = "Pig Feet";
+            //badFoods.Add("Tootsie Rolls");
+
+
+            //List<int> luckyNums = new List<int>();
+            //luckyNums.Add(13);
+            //luckyNums.Add(8);
+            //luckyNums.Add(44);
+            //luckyNums.Add(7);
+
+            //List<string> faveFilms = new List<string>() { "Wall-E", "Superwoman", "Casablanca" };
+            //faveFilms.Insert(0, "The Big Sick");
+            //faveFilms.Insert(3, "Dumbo");
+            //for (int counter = 0; counter < faveFilms.Count; counter++)
+            //{
+            //    Console.WriteLine(faveFilms[counter]);
+            //}
+
+
+            //foreach (string film in faveFilms)
+            //{
+            //    Console.WriteLine(film);
+            //}
+
+            //faveFilms.Remove("casablanca");
+            //faveFilms.Remove("superwoman");
+
+            //foreach (string movie in faveFilms)
+            //{
+            //    Console.WriteLine(movie);
+            //}
+
+            //List<string> animals = new List<string>();
+            //animals.Add("monkey");
+            //animals.Add("parrot");
+            //animals.Add("tiger");
+            //animals.Add("lion");
+            //animals.Add("bear");
+            //foreach(string killer in animals)
+            //{
+            //    Console.WriteLine(killer);
+            //}
+
+            //List<bool> boolList = new List<bool>() { true, false, false, true, false };
+            //foreach (bool rain in boolList)
+            //{
+            //    Console.WriteLine(rain);
+            //    if (rain == true)
+            //    {
+            //        Console.WriteLine("Better bring an umbrella!");
+            //    }
+            //   else
+            //    {
+            //        Console.WriteLine("No rain today, enjoy the sun!");
+            //    }
+
+            //for(int counter = 0; counter < boolList.Count; counter++)
+            //{
+            //    if (boolList[counter] == true)
+            //    {
+            //        Console.WriteLine("Bring an umbrella!!");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Enjoy the sun!");
+            //    }
+
+            List<int> numberList = new List<int>() { 1, 23, 9, 77, 922, 6, 32, 63, 14, 5 };
+
+            Console.WriteLine(numberList.Contains(23));
+            Console.WriteLine(numberList.Contains(77));
+            Console.WriteLine(numberList.Contains(15));
+
+            numberList.Remove(77);
+            numberList.Remove(32);
+            numberList.Remove(6);
+            Console.WriteLine(numberList.Contains(23));
+            Console.WriteLine(numberList.Contains(77));
+            Console.WriteLine(numberList.Contains(15));
+
+       //DICTIONAIRES- a data collection of key value pairs, all keys are same data type 
+            //and all values are the same data type. 
+            // each key is a unique identifier. Values do not have to be unique. 
+
+       
+
+
+
+
+
+        }
+        }
+    }
+
